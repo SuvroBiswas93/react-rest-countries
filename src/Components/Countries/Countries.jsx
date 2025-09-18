b82b4f4894396b1043db702fe0d635909f4d9ead
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-19 03:03:39 +0600

    Load data and show it in UI

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -3,15 +3,15 @@ import Country from '../Country/Country';
 
 const Countries = ({promiseFetch}) => {
     const countriesData = use(promiseFetch)
-    console.log(countriesData)
+    // console.log(countriesData)
     const countries = countriesData.countries
-    console.log(countries)
+    // console.log(countries)
     
     return (
         <div>
             <h1>Rest Countries:{countries.length} </h1>
             {
-                Countries.map((country) =><Country country={country}></Country>)
+                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
             }
         </div>
     );
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    const [] = country
+    // console.log(country)
+    // const [] = country
     return (
         <div>
-           <h3>Name : </h3> 
+           <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+           <h2>Name : {country.name.common}  </h2> 
+           <h3>Population : {country.population.population}</h3>
+           <h3>Area : {country.area.area} Sqkm</h3>
+           <h3>Continent : {country.continents.continents[0]} </h3>
+           
+            
+            
+           
         </div>
     );
 };
